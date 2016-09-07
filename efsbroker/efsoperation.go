ff0ab2dc0bef0f7878a8502158272f44004d73e2
Luke Woydziak <luke.woydziak@emc.com>
2016-09-07 09:56:43 -0700

    Make a fake operation. [#129734351](https://www.pivotaltracker.com/story/show/129734351)
    Signed-off-by: Paul Warren <paul.warren@emc.com>

--- efsbroker/efsoperation.go
+++ efsbroker/efsoperation.go
@@ -2,20 +2,22 @@ package efsbroker
 
 import "code.cloudfoundry.org/lager"
 
+//go:generate counterfeiter -o efsfakes/fake_operation.go . Operation
+
 type Operation interface {
 	Execute()
 }
 
 func NewProvisionOperation(underlying *broker, logger lager.Logger, fsID string) Operation {
-	return &provisionOperation{ underlying, logger, fsID, }
+	return &provisionOperation{underlying, logger, fsID}
 }
 
 type provisionOperation struct {
 	underlying *broker
-	logger lager.Logger
-	fsID string
+	logger     lager.Logger
+	fsID       string
 }
 
-func (o *provisionOperation) Execute ()  {
+func (o *provisionOperation) Execute() {
 	o.underlying.createMountTargets(o.logger, o.fsID)
 }
